c5e4757c56787fec11a5bae125b128782f8e7144
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2024-05-04 12:57:48 -0300

    Init project

---
+++ src/main/java/com/pede/ai/PedeAiApplication.java
@@ -0,0 +1,13 @@
+package com.pede.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pede/ai/PedeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pede.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
